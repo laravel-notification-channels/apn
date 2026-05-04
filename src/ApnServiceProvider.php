6392a8ed0fafb7d97da5424fd3703ac8eaf398ab
Dwight Watson <dwightwatson@users.noreply.github.com>
2026-05-05 08:45:40 +1000

    Give token it's own binding

--- src/ApnServiceProvider.php
+++ src/ApnServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace NotificationChannels\Apn;
 
+use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Support\Arr;
 use Illuminate\Support\ServiceProvider;
 use Pushok\AuthProvider\Certificate;
@@ -12,21 +13,44 @@ use Pushok\Client;
 class ApnServiceProvider extends ServiceProvider
 {
     /**
+     * The token cache key.
+     */
+    const string TOKEN_CACHE_KEY = 'apn.token';
+
+    /**
+     * The token cache length.
+     */
+    const int TOKEN_CACHE_MINUTES = 20;
+
+    /**
      * Register any application services.
      *
      * @return void
      */
     public function register()
     {
-        $this->app->bind(AuthProviderInterface::class, function ($app) {
-            $options = Arr::except(
-                $app['config']['broadcasting.connections.apn'],
-                'production',
-            );
+        $this->app->bind(AuthProviderInterface::class, function (Application $app) {
+            $options = Arr::except($app['config']['broadcasting.connections.apn'], 'production');
 
-            return Arr::exists($options, 'certificate_path')
-                ? Certificate::create($options)
-                : Token::create($options);
+            if (Arr::exists($options, 'certificate_path')) {
+                return Certificate::create();
+            }
+
+            return $this->app->make(Token::class);
+        });
+
+        $this->app->bind(Token::class, function (Application $app) {
+            $token = $app['cache']->get(static::TOKEN_CACHE_KEY);
+
+            if ($token) {
+                $options = Arr::except($app['config']['broadcasting.connections.apn'], 'production');
+
+                return Token::useExisting($token, $options);
+            }
+
+            return tap(Token::create(), function (Token $token) use ($app) {
+                $app['cache']->put(static::TOKEN_CACHE_KEY, $token->get(), now()->addMintues(self::TOKEN_CACHE_MINUTES));
+            });
         });
 
         $this->app->scoped(Client::class, function ($app) {
